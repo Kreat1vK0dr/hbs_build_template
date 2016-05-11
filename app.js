688b064fc87ad272696f701179c50de6c2597da6
Kreat1vK0dr <daniel@projectcodex.co>
2016-05-11 22:19:00 +0200

    changed dependencies

--- app.js
+++ app.js
@@ -5,25 +5,15 @@ var express = require('express'),
     path = require('path'),
     mysql = require('mysql'),
     myConnection = require('express-myconnection'),
-    bodyParser = require('body-parser'),
-    sassMiddleware = require('node-sass-middleware'),
-    postcssMiddleware = require('postcss-middleware'),
-    autoprefixer = require('autoprefixer'),
-    gulp = require('gulp'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
-    gzip = require('gulp-gzip'),
-    sourcemaps = require('gulp-sourcemaps'),
-    cleanCSS = require('gulp-clean-css'),
-    sass = require('gulp-sass');
-
+    bodyParser = require('body-parser');
+    
 // Setting up database access options.
 // var dbOptions = {
 //     host: 'localhost',
-//     user: 'root',
-//     password: '1amdan13l',
+//     user: '<username',
+//     password: '<password>',
 //     port: 3306,
-//     database: 'app_name'
+//     database: '<app_name>'
 // };
 
 // Initialising express environment.
@@ -36,35 +26,6 @@ app.engine('handlebars', exphbs({
     defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
-// Setting up middleware
-app.use('/css', sassMiddleware({
-    src: path.join(__dirname, 'dev', 'sass'),
-    dest: path.join(__dirname, 'public', 'css'),
-    debug: true,
-    outputStyle: 'expanded',
-    indentedSyntax: true,
-    sourceMap:"./public/css"
-    // prefix: '/css' // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/>
-}));
-
-// app.use(postcssMiddleware({
-//     src: function (req) {
-//         return path.join(req.path);
-//     },
-//     plugins: [autoprefixer({
-//         browsers: ['> 1%', 'IE 7', 'last 2 versions'],
-//         cascade: false
-//     })]
-// }));
-
-// gulp.task('generateScriptSourceMap', function () {
-//     gulp.src('./dev/scripts/*.js')
-//         .pipe(sourcemaps.init())
-//         .pipe(concat('main.js'))
-//         .pipe(sourcemaps.write())
-//         .pipe(gulp.dest('./public/js'));
-// });
-
 
 app.use(express.static('public'));
 // app.use(myConnection(mysql, dbOptions, 'single'));
